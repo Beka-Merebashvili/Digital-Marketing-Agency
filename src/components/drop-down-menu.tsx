1144f64ff3b1262bf975b17ecca2b943dc84a006
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-04-19 19:40:39 +0400

    finish navbar

--- src/components/drop-down-menu.tsx
+++ src/components/drop-down-menu.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 interface DropDownMenuProps {
   onClose: () => void;
-  scrollToServices: () => void; 
+ 
 }
 
-const DropDownMenu: React.FC<DropDownMenuProps> = ({ onClose, scrollToServices }) => {
+const DropDownMenu: React.FC<DropDownMenuProps> = ({ onClose }) => {
   return (
     <motion.div
       className="w-screen h-screen bg-gradient-to-b  from-neutral-50 to-neutral-400  bg-opacity-50 text-slate-300 p-6 space-y-4 absolute top-28 left-0 right-0 z-50 rounded-t-3xl"
