a47dccdc92cf4335f678fdb66b7d452b600db694
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-04-19 22:40:11 +0400

    add slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/navbar";
+import SliderOne from "@/components/ui/slider";
 import { Spotlight } from "@/components/ui/spotlight";
 import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <div className="w-full h-screen md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Navbar />
       <Spotlight className="hidden md:flex md:-top-0 left-80  " fill="white" />
       <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
@@ -22,6 +23,9 @@ export default function Home() {
         >
           Book a call
         </Link>
+        <div className="w-full pt-20">
+          <SliderOne />
+        </div>
       </div>
     </div>
   );
